75d1320bce6e1d5ec0c2994801701f817f998871
mahfudz <mahfudz@richcoremedia.com>
2012-12-17 16:59:57 +0800

    sal commit

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ include ('header.tpl.php');
 
     <section id="content">
         <h1>Welcome</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo deleniti at ad hic amet aspernatur distinctio ipsum repellat reiciendis facilis recusandae aperiam iusto ducimus culpa esse vel perspiciatis iste adipisci officia dicta repudiandae illum assumenda. Distinctio iste nostrum eum asperiores laudantium officiis autem dolor ipsa blanditiis magnam aliquid itaque minus id qui ex soluta tempore repellat corporis totam ut a provident vel praesentium voluptate dolorem obcaecati eligendi similique explicabo error voluptas quas sed consectetur natus pariatur quia sequi quo dicta! Officia inventore molestiae illum delectus ad perferendis veritatis distinctio sapiente architecto eveniet dolore sit natus ab iure numquam alias esse.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo deleniti at ad hic amet aspernatur distinctio ipsum repellat reiciendis facilis recusandae aperiam iusto ducimus culpa esse vel perspiciatis iste adipisci officia dicta repudiandae illum assumenda. Distinctio iste nostrum eum asperiores laudantium officiis autem dolor ipsa blanditiis magnam aliquid itaque minus id qui ex soluta tempore repellat corporis totam ut a provident vel praesentium voluptate dolorem obcaecati eligendi similique explicabo error voluptas quas sed consectetur natus pariatur quia sequi quo dicta! Officia inventore molestiae illum delectus ad perferendis veritatis distinctio sapiente architecto eveniet dolore sit natus ab iure numquam alias esse.teteerererere</p>
     </section>
     <!-- #content -->
 
