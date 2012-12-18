9f5fecfe143987e9588f5bc4205f886416b61717
Haryana <haryana@richcoremedia.com>
2012-12-18 12:26:19 -0800

    yana

--- guest-login.php
+++ guest-login.php
@@ -1,10 +1,67 @@
-<?php 
+<?php require_once('Connections/bookingAppCon.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? "'" . doubleval($theValue) . "'" : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+$editFormAction = $_SERVER['PHP_SELF'];
+if (isset($_SERVER['QUERY_STRING'])) {
+  $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
+}
+
+if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "form1")) {
+  $insertSQL = sprintf("INSERT INTO guest_info (Email, password) VALUES (%s, %s)",
+                       GetSQLValueString($_POST['Email'], "text"),
+                       GetSQLValueString($_POST['password'], "text"));
+
+  mysql_select_db($database_bookingAppCon, $bookingAppCon);
+  $Result1 = mysql_query($insertSQL, $bookingAppCon) or die(mysql_error());
+
+  $insertGoTo = "index.php";
+  if (isset($_SERVER['QUERY_STRING'])) {
+    $insertGoTo .= (strpos($insertGoTo, '?')) ? "&" : "?";
+    $insertGoTo .= $_SERVER['QUERY_STRING'];
+  }
+  header(sprintf("Location: %s", $insertGoTo));
+}
+
+mysql_select_db($database_bookingAppCon, $bookingAppCon);
+$query_rsLogin = "SELECT Email, password FROM guest_info";
+$rsLogin = mysql_query($query_rsLogin, $bookingAppCon) or die(mysql_error());
+$row_rsLogin = mysql_fetch_assoc($rsLogin);
+$totalRows_rsLogin = mysql_num_rows($rsLogin);
+ 
 
 /**
  * Set variable Page view on the top
  * and the page auto set page
  */
-$page_view = "Guest Log In to Booking System";
+$page_view = "Welcome to Booking System";
 
 ?>
 
@@ -18,49 +75,27 @@ $page_view = "Guest Log In to Booking System";
 include ('header.tpl.php'); 
 
 ?>
+</p>
+<h1>Guest Log In</h1>
 
 
-<section class="center">
-
-    <section id="content">
-        <h1>Guest Log In</h1>
-        <form id="form1" name="form1" method="post" action="">
-          <table width="100%" border="0" cellspacing="2" cellpadding="2">
-            <tr>
-              <td width="24%">Email</td>
-              <td width="6%">:</td>
-              <td width="70%"><label for="email"></label>
-              <input type="text" name="email" id="email" /></td>
-            </tr>
-            <tr>
-              <td>Password</td>
-              <td>:</td>
-              <td><label for="password"></label>
-              <input type="password" name="password" id="password" /></td>
-            </tr>
-            <tr>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-            </tr>
-            <tr>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-              <td><input type="submit" name="button" id="button" value="Register" /></td>
-            </tr>
-          </table>
-        </form>
-        <p>&nbsp;</p>
-    </section>
+<p>
+  <section class="center">
+    
+    <section id="content">  
+    
+    
+    
+    
+      </section>
     <!-- #content -->
-
-</section>
-<!-- <section class="container center"></section> -->
-
-
-
-
-<?php 
+  </section>
+  <!-- <section class="container center"></section> -->
+  
+  
+  
+  
+  <?php 
 
 /**
  * Include footer template
@@ -69,4 +104,29 @@ include ('header.tpl.php');
 include ('footer.tpl.php'); 
 
 
+?>
+</p>
+
+<form action="<?php echo $editFormAction; ?>" method="post" name="form1" id="form1">
+  <table align="center">
+    <tr valign="baseline">
+      <td nowrap="nowrap" align="right">Email:</td>
+      <td><input type="text" name="Email" value="" size="32" /></td>
+    </tr>
+    <tr valign="baseline">
+      <td nowrap="nowrap" align="right">Password:</td>
+      <td><input type="password" name="password" value="" size="32" /></td>
+    </tr>
+    <tr valign="baseline">
+      <td nowrap="nowrap" align="right">&nbsp;</td>
+      <td><input type="submit" value="Login" /></td>
+    </tr>
+  </table>
+  <input type="hidden" name="MM_insert" value="form1" />
+</form>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp; </p>
+<?php
+mysql_free_result($rsLogin);
 ?>
