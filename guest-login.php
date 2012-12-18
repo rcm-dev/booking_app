4592d908340e9cdd63901d38aae0f4c5147ccbd8
mahfudz <mahfudz@richcoremedia.com>
2012-12-18 12:55:07 +0800

    login bru

--- guest-login.php
+++ guest-login.php
@@ -1,9 +1,13 @@
-<?php require_once('Connections/bookingAppCon.php'); ?>
+<?php require_once('Connections/bookingAppCon.php'); ?> 
+
+ 
 <?php
 if (!function_exists("GetSQLValueString")) {
 function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
 {
-  $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
 
   $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
 
@@ -16,7 +20,7 @@ function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDe
       $theValue = ($theValue != "") ? intval($theValue) : "NULL";
       break;
     case "double":
-      $theValue = ($theValue != "") ? "'" . doubleval($theValue) . "'" : "NULL";
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
       break;
     case "date":
       $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
@@ -29,45 +33,12 @@ function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDe
 }
 }
 
-$editFormAction = $_SERVER['PHP_SELF'];
-if (isset($_SERVER['QUERY_STRING'])) {
-  $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
-}
-
-if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "form1")) {
-  $insertSQL = sprintf("INSERT INTO guest_info (Email, password) VALUES (%s, %s)",
-                       GetSQLValueString($_POST['Email'], "text"),
-                       GetSQLValueString($_POST['password'], "text"));
-
-  mysql_select_db($database_bookingAppCon, $bookingAppCon);
-  $Result1 = mysql_query($insertSQL, $bookingAppCon) or die(mysql_error());
-
-  $insertGoTo = "index.php";
-  if (isset($_SERVER['QUERY_STRING'])) {
-    $insertGoTo .= (strpos($insertGoTo, '?')) ? "&" : "?";
-    $insertGoTo .= $_SERVER['QUERY_STRING'];
-  }
-  header(sprintf("Location: %s", $insertGoTo));
-}
-
 mysql_select_db($database_bookingAppCon, $bookingAppCon);
-$query_rsLogin = "SELECT Email, password FROM guest_info";
-$rsLogin = mysql_query($query_rsLogin, $bookingAppCon) or die(mysql_error());
-$row_rsLogin = mysql_fetch_assoc($rsLogin);
-$totalRows_rsLogin = mysql_num_rows($rsLogin);
- 
+$query_login = "SELECT Email, password FROM guest_info";
+$login = mysql_query($query_login, $bookingAppCon) or die(mysql_error());
+$row_login = mysql_fetch_assoc($login);
+$totalRows_login = mysql_num_rows($login);
 
-/**
- * Set variable Page view on the top
- * and the page auto set page
- */
-$page_view = "Welcome to Booking System";
-
-?>
-
-
-
-<?php
 
 /**
  * Include header template
@@ -75,42 +46,107 @@ $page_view = "Welcome to Booking System";
 include ('header.tpl.php'); 
 
 ?>
-</p>
-<h1>Guest Log In</h1>
+<?php
+// *** Validate request to login to this site.
+if (!isset($_SESSION)) {
+  session_start();
+}
+
+$loginFormAction = $_SERVER['PHP_SELF'];
+if (isset($_GET['accesscheck'])) {
+  $_SESSION['PrevUrl'] = $_GET['accesscheck'];
+}
+
+if (isset($_POST['Email2'])) {
+  $loginUsername=$_POST['Email2'];
+  $password=$_POST['password'];
+  $MM_fldUserAuthorization = "";
+  $MM_redirectLoginSuccess = "index.php";
+  $MM_redirectLoginFailed = "login.php";
+  $MM_redirecttoReferrer = false;
+  mysql_select_db($database_bookingAppCon, $bookingAppCon);
+  
+  $LoginRS__query=sprintf("SELECT Email, password FROM guest_info WHERE Email=%s AND password=%s",
+    GetSQLValueString($loginUsername, "text"), GetSQLValueString($password, "text")); 
+   
+  $LoginRS = mysql_query($LoginRS__query, $bookingAppCon) or die(mysql_error());
+  $loginFoundUser = mysql_num_rows($LoginRS);
+  if ($loginFoundUser) {
+     $loginStrGroup = "";
+    
+    //declare two session variables and assign them
+    $_SESSION['MM_Username'] = $loginUsername;
+    $_SESSION['MM_UserGroup'] = $loginStrGroup;	      
+
+    if (isset($_SESSION['PrevUrl']) && false) {
+      $MM_redirectLoginSuccess = $_SESSION['PrevUrl'];	
+    }
+    header("Location: " . $MM_redirectLoginSuccess );
+  }
+  else {
+    header("Location: ". $MM_redirectLoginFailed );
+  }
+}
+?>
+<script src="SpryAssets/SpryValidationTextField.js" type="text/javascript"></script>
+<link href="SpryAssets/SpryValidationTextField.css" rel="stylesheet" type="text/css">
 
 
-<p>
-  <section class="center">
-    
-    <section id="content">  
-    
-<form action="<?php echo $editFormAction; ?>" method="post" name="form1" id="form1">
-  <table align="center">
-    <tr valign="baseline">
-      <td nowrap="nowrap" align="right">Email:</td>
-      <td><input type="text" name="Email" value="" size="32" /></td>
-    </tr>
-    <tr valign="baseline">
-      <td nowrap="nowrap" align="right">Password:</td>
-      <td><input type="password" name="password" value="" size="32" /></td>
-    </tr>
-    <tr valign="baseline">
-      <td nowrap="nowrap" align="right">&nbsp;</td>
-      <td><input type="submit" value="Login" /></td>
-    </tr>
-  </table>
-  <input type="hidden" name="MM_insert" value="form1" />
-</form>    
-    
-      </section>
+
+<section class="center">
+
+    <section id="content">
+<script src="SpryAssets/SpryValidationPassword.js" type="text/javascript"></script>
+<link href="SpryAssets/SpryValidationPassword.css" rel="stylesheet" type="text/css">
+<h1>Guest Log in</h1>
+<form name="form3" method="POST" action="<?php echo $loginFormAction; ?>">
+<label><br>
+          </label>
+          <table width="278" height="184" border="0">
+            <tr>
+              <td width="109">Email</td>
+              <td width="24">:</td>
+              <td width="131"><span id="sprytextfield2">
+                <label>
+                  <input type="text" name="Email2" id="Email2">
+                </label>
+              <span class="textfieldRequiredMsg">A value is required.</span></span></td>
+            </tr>
+            <tr>
+              <td>password</td>
+              <td>:</td>
+              <td><span id="sprypassword1">
+                <label>
+                  <input type="password" name="password" id="password">
+                </label>
+              <span class="passwordRequiredMsg">A value is required.</span></span></td>
+            </tr>
+            <tr>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td><input type="submit" name="Login" id="Login" value="Submit"></td>
+            </tr>
+          </table>
+          <label><br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+</label>
+        </form>
+        <p>&nbsp;</p>
+    </section>
     <!-- #content -->
-  </section>
-  <!-- <section class="container center"></section> -->
-  
-  
-  
-  
-  <?php 
+
+</section>
+<!-- <section class="container center"></section> -->
+
+
+
+
+<?php 
 
 /**
  * Include footer template
@@ -119,13 +155,12 @@ include ('header.tpl.php');
 include ('footer.tpl.php'); 
 
 
-?>
-</p>
 
-
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp; </p>
-<?php
-mysql_free_result($rsLogin);
+mysql_free_result($login);
 ?>
+<script type="text/javascript">
+<!--
+var sprytextfield2 = new Spry.Widget.ValidationTextField("sprytextfield2");
+var sprypassword1 = new Spry.Widget.ValidationPassword("sprypassword1");
+//-->
+</script>
